86a8f8744175c30cb330c1bb4cfd1d205369f6e0
helmsTC <50932892+helmsTC@users.noreply.github.com>
2025-02-12 01:24:31 -0700

    Update snippets.sh

--- snippets.sh
+++ snippets.sh
@@ -2,6 +2,17 @@ sudo apt-get --purge remove "*cublas*" "cuda*" "nvidia-cuda*" "libcuda*"
 sudo apt-get autoremove
 sudo rm -rf /usr/local/cuda*
 
+
+export CMAKE_PREFIX_PATH=$(python -c 'import sysconfig; print(sysconfig.get_path("platlib"))')
+export TORCH_CUDA_ARCH_LIST="8.0"  # For A100
+export USE_CUDA=1
+export USE_CUDNN=1
+export CUDA_HOME=/usr/local/cuda-10.2
+export PATH=$CUDA_HOME/bin:$PATH
+export CUDNN_INCLUDE_DIR=$CUDA_HOME/include
+export CUDNN_LIB_DIR=$CUDA_HOME/lib64
+export CUDNN_LIBRARY=$CUDA_HOME/lib64/libcudnn.so
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
